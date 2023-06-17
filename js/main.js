b258a7a3f2168521f2a9c47a79eefa96df54f5fa
37533 <37533@FOXUFY>
2023-06-17 11:32:32 +0300

    2 модальное окно

--- js/main.js
+++ js/main.js
@@ -2,47 +2,89 @@
 AOS.init();
 
 var modal = document.getElementById("myModal");
+var modal2 = document.getElementById("myModal2");
 var btn = document.getElementById("openModalBtn");
+var btn2 = document.getElementById("openModalBtn2");
 var closeBtn = document.getElementsByClassName("close")[0];
-var isOpen = false; // Флаг для отслеживания состояния модального окна
+var closeBtn2 = document.getElementsByClassName("close")[1];
+var isOpen = false; // Флаг для отслеживания состояния первого модального окна
+var isOpen2 = false; // Флаг для отслеживания состояния второго модального окна
 
 btn.onclick = function() {
-  // Проверка состояния модального окна
+  // Проверка состояния первого модального окна
   if (!isOpen) {
     isOpen = true;
 
     modal.style.display = "block";
     document.body.style.overflow = "hidden"; // Блокировка прокрутки заднего фона
-  }
-};
 
-// Закрытие модального окна при нажатии на крестик
-closeBtn.onclick = function() {
-  modal.style.display = "none";
-  document.body.style.overflow = "auto"; // Включение прокрутки заднего фона
-  isOpen = false; // Сброс флага состояния модального окна
-
-  // Задержка после закрытия модального окна (в миллисекундах)
-  var delayInMilliseconds = 1000;
-  setTimeout(function() {
-    // Ваш код после закрытия модального окна
-  }, delayInMilliseconds);
-};
-
-// Закрытие модального окна при клике вне его
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-    document.body.style.overflow = "auto"; // Включение прокрутки заднего фона
-    isOpen = false; // Сброс флага состояния модального окна
-
-    // Задержка после закрытия модального окна (в миллисекундах)
-    var delayInMilliseconds = 1000;
+    // Задержка перед закрытием первого модального окна (в миллисекундах)
+    var delayInMilliseconds = 500;
     setTimeout(function() {
-      // Ваш код после закрытия модального окна
+      isOpen = false;
     }, delayInMilliseconds);
   }
 };
+
+btn2.onclick = function() {
+  // Проверка состояния второго модального окна
+  if (!isOpen2) {
+    isOpen2 = true;
+
+    modal2.style.display = "block";
+    document.body.style.overflow = "hidden"; // Блокировка прокрутки заднего фона
+
+    // Задержка перед закрытием второго модального окна (в миллисекундах)
+    var delayInMilliseconds = 500;
+    setTimeout(function() {
+      isOpen2 = false;
+    }, delayInMilliseconds);
+  }
+};
+
+// Закрытие модальных окон при нажатии на крестик
+closeBtn.onclick = function() {
+  if (isOpen) {
+    return; // Если первое модальное окно находится в процессе закрытия, прерываем обработчик
+  }
+
+  modal.style.display = "none";
+  document.body.style.overflow = "auto"; // Включение прокрутки заднего фона
+};
+
+// Закрытие второго модального окна при нажатии на крестик
+closeBtn2.onclick = function() {
+  if (isOpen2) {
+    return; // Если второе модальное окно находится в процессе закрытия, прерываем обработчик
+  }
+
+  modal2.style.display = "none";
+  document.body.style.overflow = "auto"; // Включение прокрутки заднего фона
+};
+
+// Закрытие модальных окон при клике вне них
+window.onclick = function(event) {
+  if (event.target == modal && !isOpen) {
+    modal.style.display = "none";
+    document.body.style.overflow = "auto"; // Включение прокрутки заднего фона
+  }
+
+  if (event.target == modal2 && !isOpen2) {
+    modal2.style.display = "none";
+    document.body.style.overflow = "auto"; // Включение прокрутки заднего фона
+  }
+};
+
+
+document.getElementById("searchButton2").onclick = function() {
+  var searchInput = document.getElementById("searchInput2");
+  var searchResults = document.getElementById("searchResults2");
+
+  // Выполняем поиск и отображаем результаты
+  var searchText = searchInput.value;
+  searchResults.innerHTML = "Результаты поиска для \"" + searchText + "\"";
+}
+
 
 // Инициализация Cleave.js для форматирования полей
 var phoneInput = document.getElementById('phone');
