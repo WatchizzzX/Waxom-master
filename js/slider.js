b74061fca48e90be7eb9c20b5c650746fc685990
WatchizzzX <watchizx@yandex.ru>
2023-06-17 09:50:42 +0300

    first commit

---
+++ js/slider.js
@@ -0,0 +1,23 @@
+var mainSwiper = new Swiper('.mainSwiper', {
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+    watchSlidesProgress: true,
+    loop: true
+});
+
+var postSwiper = new Swiper('.postSwiper', {
+    navigation: {
+        nextEl: '.posts-button-next',
+        prevEl: '.posts-button-prev',
+    },
+    spaceBetween: 31,
+    slidesPerView: 3,
+    loop: false
+});
